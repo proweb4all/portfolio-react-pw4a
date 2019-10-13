a7fa52b5dfa68fc6af9ee7f18426802f64c8a43e
ProWeb4All <proweb4all@narod.ru>
2019-10-14 00:03:45 +0300

    update meta tags

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -7,7 +7,7 @@ const About = () => (
         <div className='about__block'>
             <img className='about__photo' src='img/ya.jpeg' alt='Юрий Таратов'/>
             <h1 className='about__h1'>Будем знакомы, я&nbsp;Юрий&nbsp;Таратов</h1>
-            <p className='about__p'>Занимаюсь созданием красивых и&nbsp;современных веб-страниц и&nbsp;веб-приложений с&nbsp;полной адаптивностью и&nbsp;полноценной интерактивностью под&nbsp;любые устройства как по&nbsp;дизайн-макету, так и&nbsp;без&nbsp;него.</p>
+            <p className='about__p'>Занимаюсь созданием современных веб-страниц и&nbsp;веб-приложений с&nbsp;полной адаптивностью и&nbsp;полноценной интерактивностью под&nbsp;любые устройства как по&nbsp;дизайн-макету, так и&nbsp;без&nbsp;него.</p>
             <p className='about__p'>Имею 2&nbsp;высших образования, постоянно развиваю свои компетенции.</p>
             <p className='about__p'>Буду рад общению и&nbsp;любому взаимовыгодному сотрудничеству.</p>
             <h3 className='about__h3'>Мои навыки:</h3>
@@ -16,8 +16,8 @@ const About = () => (
             <p className='about__p'>Javascript, jQuery, React (Redux, MobX)</p>
             <p className='about__p'>Npm, Bower, Gulp, Webpack, Git</p>
             <p className='about__p'>UI/UX design, Photoshop, Figma, Avocode, Zeplin</p>
-            <h3 className='about__h3'>Мои контакты:</h3>
-            <ContactLinks />
+            {/* <h3 className='about__h3'>Мои контакты:</h3>
+            <ContactLinks /> */}
         </div>
     </div>
 )
--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -5,14 +5,17 @@ import './contacts.scss';
 const ContactLinks = ({outerClass}) => {
   const allStyle = `contacts-links ${outerClass}`;
   return (
-    <div className={allStyle}>
-      <a href='https://www.linkedin.com/in/yurytaratov/' className='contacts-link' target="_blank" rel="noopener noreferrer" title='Открыть профиль в LinkedIn'>LinkedIn</a>
-      <a href='https://github.com/proweb4all' className='contacts-link' target="_blank" rel="noopener noreferrer" title='Открыть GitHub репозиторий'>GitHub</a>
-      <a href='skype:rustich.ruwolf?add' className='contacts-link' target="_blank" rel="noopener noreferrer" title='Связаться в Skype'>Skype</a>
-      <a href='https://vk.com/yurytaratov' className='contacts-link' target="_blank" rel="noopener noreferrer" title='Открыть профиль в ВКонтакте'>VK</a>
-      <a href='mailto:yurytaratov@yandex.ru' className='contacts-link' target="_blank" rel="noopener noreferrer" title='Написать на email'>Email</a>
-      <a href='tel:+79106436216' className='contacts-link' target="_blank" rel="noopener noreferrer" title='Позвонить'>Phone</a>
-    </div>
+    <>
+      <div className={allStyle}>
+        <a href='https://www.linkedin.com/in/yurytaratov/' className='contacts-link icon-linkedin' target="_blank" rel="noopener noreferrer" title='Открыть профиль в LinkedIn'></a>
+        <a href='https://github.com/proweb4all' className='contacts-link icon-github' target="_blank" rel="noopener noreferrer" title='Открыть GitHub репозиторий'></a>
+        <a href='skype:rustich.ruwolf?add' className='contacts-link icon-skype' target="_blank" rel="noopener noreferrer" title='Связаться в Skype'></a>
+        <a href='https://vk.com/yurytaratov' className='contacts-link icon-vk' target="_blank" rel="noopener noreferrer" title='Открыть профиль в ВКонтакте'></a>
+        <a href='https://t.me/Yury_PW4A' className='contacts-link icon-telegram' target="_blank" rel="noopener noreferrer" title='Связаться в Telegram'></a>
+        <a href='mailto:yurytaratov@yandex.ru' className='contacts-link icon-mail' target="_blank" rel="noopener noreferrer" title='Написать на email'></a>
+        <a href='tel:+79106436216' className='contacts-link icon-phone' target="_blank" rel="noopener noreferrer" title='Позвонить'></a>
+      </div>
+    </>
   )
 }
 
